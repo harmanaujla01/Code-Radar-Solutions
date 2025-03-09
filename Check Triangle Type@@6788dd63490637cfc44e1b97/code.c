70f3cefc43109d451a6c6f2e50ac15c880e698ef
harmanaujla01 <harmanaujla916@gmail.com>
2025-03-09 22:00:29 +0530

    Solution to Check Triangle Type

---
+++ Check Triangle Type@@6788dd63490637cfc44e1b97/code.c
@@ -0,0 +1,16 @@
+// Your code here...
+#include <stdio.h>
+int main(){
+    int a;
+    int b;
+    scanf("%d %d",&a,&b);
+    if((a-b)>0){
+        printf("Loss");
+    }
+    else if((a-b)<0){
+        printf("Profit");
+    }
+    else{
+        printf("No Profit No Loss");
+    }
+}
